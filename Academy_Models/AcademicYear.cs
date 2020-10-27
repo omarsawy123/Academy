c64afe7a145428466ba1d2ee6e0de0bd425eede5
omar sawy <46130283+omarsawy123@users.noreply.github.com>
2020-10-27 06:27:32 +0200

    Academy-Models

---
+++ Academy_Models/AcademicYear.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academy_Models
+{
+    public enum AcademicYear
+    {
+        FirstYear,
+        SecondYear,
+        ThirdYear,
+        FourthYear
+    }
+}
---
+++ Academy_Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Academy_Models
+{
+    public class Student
+    {
+
+        public int id { get; set; }
+
+        [Required]
+        [MinLength(3,ErrorMessage ="Name too short")]
+        [MaxLength(20,ErrorMessage ="Name too long")]
+        public string name { get; set; }
+
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required]
+        
+        public AcademicYear AcademicYear { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
